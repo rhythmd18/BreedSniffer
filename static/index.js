713a73e43a6ccff7556ebd63da6ac14b1bb80fe8
Rhythm Dutta <109751995+rhythmd18@users.noreply.github.com>
2024-02-24 14:28:17 +0530

    Necessary changes made

--- static/index.js
+++ static/index.js
@@ -1,3 +1,19 @@
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("image-canv");
 const ctx = canvas.getContext("2d");
-const imageInput = document.getElementById("imageInput");
+const imageInput = document.getElementById("image-input");
+
+imageInput.addEventListener("change", function (event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    const img = new Image();
+    img.onload = function () {
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      document.getElementById("upload-btn").style.display = "none";
+      document.getElementById("detect-btn").removeAttribute("disabled");
+    };
+    img.src = e.target.result;
+  };
+  reader.readAsDataURL(file);
+});
