630398b3f934fddda5925c5330554b4c8d3fe137
Rhythm Dutta <109751995+rhythmd18@users.noreply.github.com>
2024-02-27 12:37:56 +0530

    Necessary changes made

--- static/index.js
+++ static/index.js
@@ -3,17 +3,19 @@ const ctx = canvas.getContext("2d");
 const imageInput = document.getElementById("image-input");
 
 imageInput.addEventListener("change", function (event) {
-  const file = event.target.files[0];
-  const reader = new FileReader();
+  const file = event.target.files[0]; // get the first file
+  const reader = new FileReader(); // create a reader
 
-  reader.onload = function (e) {
-    const img = new Image();
+  reader.onload = function () {
+    // when the reader has loaded
+    const img = new Image(); // create an image
     img.onload = function () {
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-      document.getElementById("upload-btn").style.display = "none";
-      document.getElementById("detect-btn").removeAttribute("disabled");
+      // when the image has loaded
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height); // draw the image on the canvas
+      document.getElementById("upload-btn").style.display = "none"; // hide the upload button
+      document.getElementById("detect-btn").removeAttribute("disabled"); // enable the detect button
     };
-    img.src = e.target.result;
+    img.src = reader.result; // set the image source
   };
   reader.readAsDataURL(file);
 });
